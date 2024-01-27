2c40f7aec66a3cfad35cc203b750639c71b7e629
ritchie Boodhoo <ritchieroy11@gmail.com>
2024-01-27 12:11:33 +0400

    yacht controller completed

--- app/controllers/yacht_controller.rb
+++ app/controllers/yacht_controller.rb
@@ -1,2 +1,24 @@
 class YachtController < ApplicationController
+  def index
+    @yachts = Yacht.all
+  end
+
+  def new
+    @yacht = Yacht.new
+  end
+
+  def create
+    @yacht = Yacht.new(yacht_params)
+    if @yacht.save
+      redirect_to yacht_path(@yacht)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def yacht_params
+    params.require(:yacht).permit(:price_per_day)
+  end
 end
