b2091afff9d959c7d83b0d4a6c167c52b9edea61
ritchie Boodhoo <ritchieroy11@gmail.com>
2024-01-27 15:02:08 +0400

    yacht index done

--- app/models/yacht.rb
+++ app/models/yacht.rb
@@ -1,4 +1,5 @@
 class Yacht < ApplicationRecord
   has_many :bookings
-  belongs_to :users
+  # belongs_to :users
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_26_160628) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_27_090515) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_26_160628) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.string "name"
     t.index ["user_id"], name: "index_yachts_on_user_id"
   end
 
